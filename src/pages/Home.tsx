07167962964fa5c0888056eb50463ff7969f09ef
wesleyeagles <84718917+wesleyeagles@users.noreply.github.com>
2022-09-17 16:11:34 -0300

    Add files via upload

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,67 @@ export function Home() {
 
     const { data, loading, error } = useGetImagesQuery()
 
-    if (data) {
+    if (!data && loading) {
+
+        return (
+            <Box bg='#0a0a23' w='100%' h='100vh'>
+                <Container maxW={{lg: '5xl', '2xl': '7xl'}} h='100%'>
+                    <Heading color='#FFF' paddingTop={6}>
+                        Galeria de fotos da Mariana
+                    </Heading>
+                    <Box w='100%' h='80%' bg='#FFF' marginTop={8} borderRadius='xl' overflow='auto'>
+                        <Box  padding={6} bg='#1f1b3d' display='flex' flexWrap='wrap' gap={6}>
+                            <Skeleton isLoaded={!loading}>
+                            <Image className='flex-none' borderRadius={10} minW={{sm: '350px', md: '290px'}} w={{sm: '350px', md: 290}} h={400} src=''/>
+                            </Skeleton>
+
+                            <Skeleton isLoaded={!loading}>
+                            <Image className='flex-none' borderRadius={10} minW={{sm: '350px', md: '290px'}} w={{sm: '350px', md: 290}} h={400} src=''/>
+                            </Skeleton>
+
+                            <Skeleton isLoaded={!loading}>
+                            <Image className='flex-none' borderRadius={10} minW={{sm: '350px', md: '290px'}} w={{sm: '350px', md: 290}} h={400} src=''/>
+                            </Skeleton>
+
+                            <Skeleton isLoaded={!loading}>
+                            <Image className='flex-none' borderRadius={10} minW={{sm: '350px', md: '290px'}} w={{sm: '350px', md: 290}} h={400} src=''/>
+                            </Skeleton>
+
+                            <Skeleton isLoaded={!loading}>
+                            <Image className='flex-none' borderRadius={10} minW={{sm: '350px', md: '290px'}} w={{sm: '350px', md: 290}} h={400} src=''/>
+                            </Skeleton>
+
+                            <Skeleton isLoaded={!loading}>
+                            <Image className='flex-none' borderRadius={10} minW={{sm: '350px', md: '290px'}} w={{sm: '350px', md: 290}} h={400} src=''/>
+                            </Skeleton>
+
+                            <Skeleton isLoaded={!loading}>
+                            <Image className='flex-none' borderRadius={10} minW={{sm: '350px', md: '290px'}} w={{sm: '350px', md: 290}} h={400} src=''/>
+                            </Skeleton>
+
+                            <Skeleton isLoaded={!loading}>
+                            <Image className='flex-none' borderRadius={10} minW={{sm: '350px', md: '290px'}} w={{sm: '350px', md: 290}} h={400} src=''/>
+                            </Skeleton>
+
+                            <Skeleton isLoaded={!loading}>
+                            <Image className='flex-none' borderRadius={10} minW={{sm: '350px', md: '290px'}} w={{sm: '350px', md: 290}} h={400} src=''/>
+                            </Skeleton>
+
+                            <Skeleton isLoaded={!loading}>
+                            <Image className='flex-none' borderRadius={10} minW={{sm: '350px', md: '290px'}} w={{sm: '350px', md: 290}} h={400} src=''/>
+                            </Skeleton>
+
+                            <Skeleton isLoaded={!loading}>
+                            <Image className='flex-none' borderRadius={10} minW={{sm: '350px', md: '290px'}} w={{sm: '350px', md: 290}} h={400} src=''/>
+                            </Skeleton>
+
+                        </Box>
+                    </Box>
+                </Container>
+            </Box>
+        )
+    }
+         {
         return (
             <Box bg='#0a0a23' w='100%' h='100vh'>
                 <Container maxW={{lg: '5xl', '2xl': '7xl'}} h='100%'>
@@ -27,64 +87,8 @@ export function Home() {
                 </Container>
             </Box>
         )
-    } else if (!data && loading) {
-        return (
-            <Box bg='#0a0a23' w='100%' h='100vh'>
-                <Container maxW={{lg: '5xl', '2xl': '7xl'}} h='100%'>
-                    <Heading color='#FFF' paddingTop={6}>
-                        Galeria de fotos da Mariana
-                    </Heading>
-                    <Box w='100%' h='80%' bg='#FFF' marginTop={8} borderRadius='xl' overflow='auto'>
-                        <Box  padding={6} bg='#1f1b3d' display='flex' flexWrap='wrap' gap={6}>
-                            <Skeleton isLoaded={!loading}>
-                            <Image className='flex-none' borderRadius={10} minW={{sm: '350px', md: '290px'}} w={{sm: '350px', md: 290}} h={400} src=''/>
-                            </Skeleton>
-
-                            <Skeleton isLoaded={!loading}>
-                            <Image className='flex-none' borderRadius={10} minW={{sm: '350px', md: '290px'}} w={{sm: '350px', md: 290}} h={400} src=''/>
-                            </Skeleton>
-
-                            <Skeleton isLoaded={!loading}>
-                            <Image className='flex-none' borderRadius={10} minW={{sm: '350px', md: '290px'}} w={{sm: '350px', md: 290}} h={400} src=''/>
-                            </Skeleton>
-
-                            <Skeleton isLoaded={!loading}>
-                            <Image className='flex-none' borderRadius={10} minW={{sm: '350px', md: '290px'}} w={{sm: '350px', md: 290}} h={400} src=''/>
-                            </Skeleton>
-
-                            <Skeleton isLoaded={!loading}>
-                            <Image className='flex-none' borderRadius={10} minW={{sm: '350px', md: '290px'}} w={{sm: '350px', md: 290}} h={400} src=''/>
-                            </Skeleton>
-
-                            <Skeleton isLoaded={!loading}>
-                            <Image className='flex-none' borderRadius={10} minW={{sm: '350px', md: '290px'}} w={{sm: '350px', md: 290}} h={400} src=''/>
-                            </Skeleton>
-
-                            <Skeleton isLoaded={!loading}>
-                            <Image className='flex-none' borderRadius={10} minW={{sm: '350px', md: '290px'}} w={{sm: '350px', md: 290}} h={400} src=''/>
-                            </Skeleton>
-
-                            <Skeleton isLoaded={!loading}>
-                            <Image className='flex-none' borderRadius={10} minW={{sm: '350px', md: '290px'}} w={{sm: '350px', md: 290}} h={400} src=''/>
-                            </Skeleton>
-
-                            <Skeleton isLoaded={!loading}>
-                            <Image className='flex-none' borderRadius={10} minW={{sm: '350px', md: '290px'}} w={{sm: '350px', md: 290}} h={400} src=''/>
-                            </Skeleton>
-
-                            <Skeleton isLoaded={!loading}>
-                            <Image className='flex-none' borderRadius={10} minW={{sm: '350px', md: '290px'}} w={{sm: '350px', md: 290}} h={400} src=''/>
-                            </Skeleton>
-
-                            <Skeleton isLoaded={!loading}>
-                            <Image className='flex-none' borderRadius={10} minW={{sm: '350px', md: '290px'}} w={{sm: '350px', md: 290}} h={400} src=''/>
-                            </Skeleton>
-
-                        </Box>
-                    </Box>
-                </Container>
-            </Box>
-        )
+    } {
+        
     }
     
 }
