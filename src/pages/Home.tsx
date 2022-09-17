6f84a3b5d1a6696ab1dbe256d2f19350129d5efb
wesleyeagles <84718917+wesleyeagles@users.noreply.github.com>
2022-09-17 15:05:05 -0300

    Add files via upload

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,10 +9,6 @@ export function Home() {
 
     const { data, loading, error } = useGetImagesQuery()
 
-    useEffect(() => {
-        console.log(loading)
-    }, [loading])
-
     return (
         <Box bg='#0a0a23' w='100%' h='100vh'>
             <Container maxW={{lg: '5xl', '2xl': '7xl'}} h='100%'>
@@ -22,9 +18,7 @@ export function Home() {
                 <Box w='100%' h='80%' bg='#FFF' marginTop={8} borderRadius='xl' overflow='auto'>
                     <Box  padding={6} bg='#1f1b3d' display='flex' flexWrap='wrap' gap={6}>
                        {data?.assets.map(asset => (
-       
                         <Image className='flex-none' borderRadius={10} minW={{sm: '350px', md: '100%'}} w={{sm: '350px', md: 290}} h={400} src={asset.url}/>
-           
                        ))}
                     </Box>
                 </Box>
